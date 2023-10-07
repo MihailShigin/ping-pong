33d02aaa38807e5c28a64b04134f59c98a0340d9
MihailShigin <146520087+MihailShigin@users.noreply.github.com>
2023-10-07 10:50:48 +0300

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -1,14 +1,51 @@
 from pygame import *
 
+#
 window = display.set_mode((700, 500))
-display.set_caption('')
+display.set_caption('Ping-pong')
 background = transform.scale(image.load('fon.jpg'), (700, 500))
 
-game = True
+keys_pressed = key.get_pressed()
 
+sprite1 = transform.scale(
+    image.load('spritee.png'),
+    (110, 130)
+)
+x_s1, y_s1 = 2, 150
+
+sprite2 = transform.scale(
+    image.load('spritee.png'),
+    (110, 130)
+)
+x_s2, y_s2 = 600, 150
+
+
+clock = time.Clock()
+FPS = 60
+
+
+game = True
 while game:
     window.blit(background, (0,0))
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+    keys_pressed = key.get_pressed()
+    if keys_pressed[K_UP] and y_s1 > 5:
+        y_s1 -= 10
+    if keys_pressed[K_DOWN] and y_s1 < 400:
+        y_s1 += 10
+    if keys_pressed[K_s] and y_s2 < 400:
+        y_s2 += 10
+    if keys_pressed[K_w] and y_s2 > 5:
+        y_s2 -= 10
+
+
+
+    window.blit(background, (0,0))
+    window.blit(sprite1, (x_s1, y_s1))
+    window.blit(sprite2, (x_s2, y_s2))
+
     display.update()
+    clock.tick(FPS)
